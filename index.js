4af5b3b2a6c283fadf238c3a2a42a7e1a79c39ab
siddikur-dev <siddikur.dev@gmail.com>
2025-09-05 08:00:51 +0600

    word showing card designed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const lessonVocabulary = (lessons) => {
     // create element
     const createDiv = document.createElement("div");
     createDiv.innerHTML = `
-    <button class="btn btn-outline btn-primary btn-sm flex items-center gap-1">
+    <button class="btn btn-outline btn-primary btn-sm flex items-center gap-1 ">
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
 stroke-linejoin="round" class="lucide lucide-book-open-icon lucide-book-open w-4">
