02f0155a8059eabcc368ad1ba30a8fc89159a32c
siddikur-dev <siddikur.dev@gmail.com>
2025-09-05 08:08:11 +0600

    data fetched

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const lessonVocabulary = (lessons) => {
     // create element
     const createDiv = document.createElement("div");
     createDiv.innerHTML = `
-    <button class="btn btn-outline btn-primary btn-sm flex items-center gap-1 ">
+    <button onClick="loadWard(${lesson.level_no})" class="btn btn-outline btn-primary btn-sm flex items-center gap-1 ">
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
 stroke-linejoin="round" class="lucide lucide-book-open-icon lucide-book-open w-4">
@@ -29,4 +29,23 @@ ${lesson.level_no}
   });
 };
 
+const loadWard = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => UiShowingWord(data.data));
+};
+
+const UiShowingWord = (words) => {
+  //get Level-word-container div
+  const levelWordDiv = document.getElementById("Level-word-container");
+  levelWordDiv.innerHTML = "";
+  words.map((word) => {
+    const createDiv=document.createElement("div")
+    createDiv.innerHTML=`
+    
+    `
+    console.log(word);
+  });
+};
 loadLesson();
