b9b0b541ff9f526baf4dbf3d59f33aa5260963f5
siddikur-dev <siddikur.dev@gmail.com>
2025-09-09 21:42:39 +0600

    validation checked

--- Login.js
+++ Login.js
@@ -1,5 +1,17 @@
-const loginBtn = document.getElementById("getStartedBtn");
+const getValue = (id) => {
+  const value = document.getElementById(id).value;
+  value * 1;
+  return value;
+};
 
-loginBtn.addEventListener("click", function () {
-  window.location.href = "./index.html";
+document.getElementById("getStartedBtn").addEventListener("click", function () {
+  //get user name and password
+  const userName = getValue("userName");
+  const password = getValue("password");
+  console.log(userName, password);
+  if (userName === "siddikur-dev" && password === "1234") {
+    window.location.href = "./index.html";
+  } else {
+    alert("please give correct password");
+  }
 });
